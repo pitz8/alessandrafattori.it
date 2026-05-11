03ca9e573db6ba1993b50d890c5521b3817ced78
Pietro <pietromambelli8@gmail.com>
2026-05-11 19:38:28 +0200

    first commit

---
+++ wellness/schedule.js
@@ -0,0 +1,44 @@
+const scheduleData = [
+  {
+    "day": "Lunedì",
+    "class": "Mat Pilates",
+    "time": "19:30 – 20:30",
+    "location": "Scuole Ferrari",
+    "type": "Pilates"
+  },
+  {
+    "day": "Martedì",
+    "class": "Total Body",
+    "time": "09:30 – 10:15",
+    "location": "FitUp",
+    "type": "Fitness"
+  },
+  {
+    "day": "Martedì",
+    "class": "Pilates Barre",
+    "time": "19:30 – 20:30",
+    "location": "Scuole Ferrari",
+    "type": "Pilates"
+  },
+  {
+    "day": "Venerdì",
+    "class": "Yoga",
+    "time": "09:30 – 10:15",
+    "location": "FitUp",
+    "type": "Yoga"
+  },
+  {
+    "day": "Venerdì",
+    "class": "Pilates Strength",
+    "time": "12:30 – 13:15",
+    "location": "FitUp",
+    "type": "Pilates"
+  },
+  {
+    "day": "Venerdì",
+    "class": "Vinyasa Yoga",
+    "time": "19:30 – 20:30",
+    "location": "Scuole Ferrari",
+    "type": "Yoga"
+  }
+];
